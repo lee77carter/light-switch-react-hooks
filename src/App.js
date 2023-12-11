ec00216f43f175043f14b41fc1fc16b0d43b3e5d
lee_c <gitleelee@gmail.com>
2023-12-11 15:41:26 -0800

    Transforming a class component into a functional component.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class App extends Component {
-  state = { on: true };
+const App = () => {
+  const [isOn, setIsOn] = useState(true);
 
-  toggleLight = () => {
-    const isOn = this.state.on;
-    this.setState({ on: !isOn });
+  const toggleLight = () => {
+    setIsOn(!isOn);
   };
 
-  render() {
-    let wallClass = "wall off";
-    if (this.state.on) {
-      wallClass = "wall on";
-    }
-    return (
-      <div className={wallClass}>
-        <div className="switch-plate">
-          <div className="screw" />
-          <div className="switch" onClick={this.toggleLight}>
-            <div className="switch-handle" />
-          </div>
-          <div className="screw" />
-        </div>
-      </div>
-    );
+  let wallClass = "wall off";
+  if (isOn) {
+    wallClass = "wall on";
   }
-}
+  return (
+    <div className={wallClass}>
+      <div className="switch-plate">
+        <div className="screw" />
+        <div className="switch" onClick={toggleLight}>
+          <div className="switch-handle" />
+        </div>
+        <div className="screw" />
+      </div>
+    </div>
+  );
+};
 
 export default App;
